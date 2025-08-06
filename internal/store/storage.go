4c3b4e551abbb2a2153e62336d0caa24bfc55e3a
Abuhurrara <abuhurrara1093@gmail.com>
2025-08-06 19:41:43 +0500

    add create query

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -10,13 +10,13 @@ type Storage struct {
 		Create(ctx context.Context) error
 	}
 	Posts interface {
-		Create(ctx context.Context) error
+		Create(ctx context.Context, post *Posts) error
 	}
 }
 
 func NewStorage(db *sql.DB) Storage {
 	return Storage{
 		Users: &UsersStore{db},
-		Posts: &PostsStore{db},
+		Posts: &PostStore{db},
 	}
 }

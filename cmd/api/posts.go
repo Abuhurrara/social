4cb31d96516612550f2e74eb63479da917311bf7
Abuhurrara <abuhurrara1093@gmail.com>
2025-08-17 20:25:10 +0500

    add vallidation to struct

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -2,21 +2,27 @@ package main
 
 import (
 	"errors"
-	"github.com/Abuhurrara/social/internal/store"
-	"github.com/go-chi/chi/v5"
 	"net/http"
 	"strconv"
+
+	"github.com/Abuhurrara/social/internal/store"
+	"github.com/go-chi/chi/v5"
 )
 
 type CreatePostPayload struct {
-	Title   string   `json:"title"`
-	Content string   `json:"content"`
+	Title   string   `json:"title" validate:"required,max=100"`
+	Content string   `json:"content" validate:"required,max=1000"`
 	Tags    []string `json:"tags"`
 }
 
 func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request) {
 	var payload CreatePostPayload
 	if err := readJson(w, r, &payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
 		app.badRequestResponse(w, r, err)
 		return
 	}

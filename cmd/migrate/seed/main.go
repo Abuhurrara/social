4bc928cb8ac5841c52b19733ee17356683ef4352
Abuhurrara <abuhurrara1093@gmail.com>
2025-09-01 19:50:47 +0500

    add seed/main.go

---
+++ cmd/migrate/seed/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/Abuhurrara/social/internal/db"
+	"github.com/Abuhurrara/social/internal/env"
+	store2 "github.com/Abuhurrara/social/internal/store"
+	"log"
+)
+
+func main() {
+	addr := env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/socialnetwork?sslmode=disable")
+	conn, err := db.New(addr, 3, 3, "15m")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+
+	store := store2.NewStorage(conn)
+
+	db.Seed(store)
+}

590e4fbda94b81a20db19af7d0985a22a8384d25
Abuhurrara <abuhurrara1093@gmail.com>
2025-09-01 21:50:44 +0500

    complete assignment of adding comment to post

---
+++ cmd/api/comments.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/Abuhurrara/social/internal/store"
+	"net/http"
+)
+
+type createCommentPayload struct {
+	Content string `json:"content" validate:"required,max=500"`
+}
+
+func (app *application) createCommentHandler(w http.ResponseWriter, r *http.Request) {
+	post := getPostFromContext(r)
+
+	var payload createCommentPayload
+	if err := readJson(w, r, &payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Check if post exists
+	_, err := app.store.Posts.GetByID(r.Context(), post.ID)
+	if err != nil {
+		if errors.Is(err, store.ErrNotFound) {
+			app.notFoundResponse(w, r, err)
+			return
+		} else {
+			app.internalServerError(w, r, err)
+		}
+		return
+	}
+
+	comment := &store.Comments{
+		PostID:  post.ID,
+		Content: payload.Content,
+		UserID:  post.UserID,
+	}
+
+	fmt.Println(comment)
+
+	if err := app.store.Comments.Create(r.Context(), comment); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	if err := app.JsonResponse(w, http.StatusOK, comment); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+}

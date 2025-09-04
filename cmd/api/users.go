be82c3b00c0542521d630427f571c88ed028ba3c
Abuhurrara <abuhurrara1093@gmail.com>
2025-09-04 19:48:24 +0500

    Add get user in middleware

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"github.com/Abuhurrara/social/internal/store"
 	"github.com/go-chi/chi/v5"
@@ -8,29 +9,52 @@ import (
 	"strconv"
 )
 
+type UserKey string
+
+const userCtxKey UserKey = "user"
+
 func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
-	idParam := chi.URLParam(r, "userID")
-	userID, err := strconv.ParseInt(idParam, 10, 64)
-	if err != nil {
-		app.internalServerError(w, r, err)
-		return
-	}
-
-	ctx := r.Context()
-
-	user, err := app.store.Users.GetByID(ctx, userID)
-	if err != nil {
-		switch {
-		case errors.Is(err, store.ErrNotFound):
-			app.notFoundResponse(w, r, err)
-		default:
-			app.internalServerError(w, r, err)
-		}
-		return
-	}
+	user := getUserFromContext(r)
 
 	if err := app.JsonResponse(w, http.StatusOK, user); err != nil {
 		app.internalServerError(w, r, err)
 		return
 	}
+}
+
+func (app *application) followUserHandler(w http.ResponseWriter, r *http.Request) {}
+
+func (app *application) unfollowUserHandler(w http.ResponseWriter, r *http.Request) {}
+
+func (app *application) userContextMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		idParam := chi.URLParam(r, "userID")
+		userID, err := strconv.ParseInt(idParam, 10, 64)
+		if err != nil {
+			app.internalServerError(w, r, err)
+			return
+		}
+
+		ctx := r.Context()
+
+		user, err := app.store.Users.GetByID(ctx, userID)
+		if err != nil {
+			switch {
+			case errors.Is(err, store.ErrNotFound):
+				app.notFoundResponse(w, r, err)
+			default:
+				app.internalServerError(w, r, err)
+			}
+			return
+		}
+
+		ctx = context.WithValue(ctx, userCtxKey, user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+
+}
+
+func getUserFromContext(r *http.Request) *store.User {
+	user, _ := r.Context().Value(userCtxKey).(*store.User)
+	return user
 }

029b0e4b9e06aeadbe5c9ea09ae5da0a09d0d0d9
Abuhurrara <abuhurrara1093@gmail.com>
2025-09-08 20:16:05 +0500

    add sql indexes

---
+++ cmd/migrate/migrations/000008_add_indexes.down.sql
@@ -0,0 +1,12 @@
+DROP INDEX IF EXISTS idx_post_title;
+
+DROP INDEX IF EXISTS idx_post_tags;
+
+DROP INDEX IF EXISTS idx_user_username;
+
+DROP INDEX IF EXISTS idx_posts_user_id;
+
+DROP INDEX IF EXISTS idx_comments_post_id;
+
+DROP INDEX IF EXISTS idx_comments_content;
+

c9decda44278b1327f71ceb12b685985fd413f90
Abuhurrara <abuhurrara1093@gmail.com>
2025-09-10 02:39:26 +0500

    complete feed algortihm

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -8,7 +8,7 @@ func (app *application) getUserFeed(w http.ResponseWriter, r *http.Request) {
 	// pagination, filters
 	ctx := r.Context()
 
-	feed, err := app.store.Posts.GetUserFeed(ctx, int64(42))
+	feed, err := app.store.Posts.GetUserFeed(ctx, int64(4))
 	if err != nil {
 		app.internalServerError(w, r, err)
 		return

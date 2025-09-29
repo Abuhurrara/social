16ee185d1ca43a168ed691a8a23c5f7e9212aaec
Abuhurrara <abuhurrara1093@gmail.com>
2025-09-29 15:23:57 +0500

    SN-9: add role id while creating users

--- internal/store/users.go
+++ internal/store/users.go
@@ -23,6 +23,7 @@ type User struct {
 	Password  password `json:"-"`
 	CreatedAt string   `json:"created_at"`
 	IsActive  bool     `json:"is_active"`
+	RoleID    int64    `json:"role_id"`
 }
 
 type password struct {
@@ -52,7 +53,7 @@ type UserStore struct {
 
 func (s *UserStore) Create(ctx context.Context, tx *sql.Tx, user *User) error {
 	query := `
-		INSERT INTO users (username, password, email) VALUES($1, $2, $3) RETURNING id, created_at
+		INSERT INTO users (username, password, email, role_id) VALUES($1, $2, $3, $4) RETURNING id, created_at
 `
 	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
 	defer cancel()
@@ -63,6 +64,7 @@ func (s *UserStore) Create(ctx context.Context, tx *sql.Tx, user *User) error {
 		user.Username,
 		user.Password.hash,
 		user.Email,
+		user.RoleID,
 	).Scan(&user.ID, &user.CreatedAt)
 	if err != nil {
 		switch {

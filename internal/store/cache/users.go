0b8f0c85b79e31a00ac21df984160fbbd7da1219
Abuhurrara <abuhurrara1093@gmail.com>
2025-09-30 18:47:35 +0500

    SN-10: implement get func

--- internal/store/cache/users.go
+++ internal/store/cache/users.go
@@ -2,6 +2,8 @@ package cache
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/Abuhurrara/social/internal/store"
 	"github.com/go-redis/redis/v8"
 )
@@ -11,9 +13,23 @@ type UserStore struct {
 }
 
 func (s *UserStore) Get(ctx context.Context, userID int64) (*store.User, error) {
+	cacheKey := fmt.Sprintf("user-%v", userID)
+	data, err := s.rdb.Get(ctx, cacheKey).Result()
+	if err != nil {
+		return nil, err
+	}
 
+	var user store.User
+	if data != "" {
+		err = json.Unmarshal([]byte(data), &user)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &user, nil
 }
 
 func (s *UserStore) Set(ctx context.Context, user *store.User) error {
-	
+
 }

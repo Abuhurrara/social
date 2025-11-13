fabec4e7e3fb12b659d14e6dda5535bb149248dd
Abuhurrara <abuhurrara1093@gmail.com>
2025-11-13 16:14:01 +0500

    SN-11: add mock Cache store

--- cmd/api/test_utils.go
+++ cmd/api/test_utils.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/Abuhurrara/social/internal/store"
+	"github.com/Abuhurrara/social/internal/store/cache"
 	"go.uber.org/zap"
 )
 
@@ -11,10 +12,12 @@ func NewTestApplication(t *testing.T) *application {
 	t.Helper()
 
 	logger := zap.Must(zap.NewProduction()).Sugar()
-	mockStore := store.NewMockStore{}
+	mockStore := store.NewMockStore()
+	mockCacheStore := cache.NewMockStore()
 
 	return &application{
-		logger: logger,
-		store:  mockStore,
+		logger:       logger,
+		store:        mockStore,
+		cacheStorage: mockCacheStore,
 	}
 }
--- cmd/api/users_test.go
+++ cmd/api/users_test.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestGetUser(t *testing.T) {
+	app := NewTestApplication(t)
+	mux := app.mount()
+
 	t.Run("Should not allow unauthenticated requests", func(t *testing.T) {
 		req, err := http.NewRequest(http.MethodGet, "/v1/users/1", nil)
 		if err != nil {
 			t.Fatal(err)
 		}
+
+		rr := httptest.NewRecorder()
+		mux.ServeHTTP(rr, req)
 	})
 }

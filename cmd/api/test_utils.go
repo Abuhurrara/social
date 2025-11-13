970aeafcbe1b24a9bb2c340f521c80da9fd8e1c8
Abuhurrara <abuhurrara1093@gmail.com>
2025-11-13 16:04:56 +0500

    SN-11: add MockUserStore

--- cmd/api/test_utils.go
+++ cmd/api/test_utils.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/Abuhurrara/social/internal/store"
+	"go.uber.org/zap"
+)
+
+func NewTestApplication(t *testing.T) *application {
+	t.Helper()
+
+	logger := zap.Must(zap.NewProduction()).Sugar()
+	mockStore := store.NewMockStore{}
+
+	return &application{
+		logger: logger,
+		store:  mockStore,
+	}
+}
